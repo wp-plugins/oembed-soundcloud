aeddf3f6abaf28d76271e33cd7113b92dba948a0
shutterbrandon <shutterbrandon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-04 21:56:10 +0000

    Remove unneeded line break.
    git-svn-id: https://plugins.svn.wordpress.org/oembed-soundcloud/trunk@960209 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- soundcloud.php
+++ soundcloud.php
@@ -3,7 +3,7 @@
 Plugin Name: Soundcloud oEmbed
 Plugin URI: http://www.brandonshutter.com
 Description: Enables the ability to paste a SoundCloud URL in it's own line and for oEmbed to make it play.
-Version: 1.0
+Version: 1.1
 Author: Brandon Shutter
 Author URI: http://www.brandonshutter.com
 */
@@ -14,6 +14,5 @@ function add_oembed_soundcloud(){
 	wp_oembed_add_provider( 'http://soundcloud.com/*', 'http://soundcloud.com/oembed' );
 }
 add_action('init','add_oembed_soundcloud');
-
 
 ?>
