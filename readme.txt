dd962391e1feaf458667de0449ee427ecdf4fe54
shutterbrandon <shutterbrandon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-09 17:30:23 +0000

    Update to Version 2
    git-svn-id: https://plugins.svn.wordpress.org/oembed-soundcloud/trunk@1004634 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -2,8 +2,8 @@
 Contributors: http://profiles.wordpress.org/shutterbrandon
 Tags: Soundcloud, oEmbed
 Requires at least: 3.3
-Tested up to: 3.9
-Stable tag: 1.1
+Tested up to: 4.0
+Stable tag: 2.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -30,5 +30,7 @@ Copy and paste a soundcloud URL where you want the player to show up.
 1. http://i.imgur.com/LwuvfNT.jpg
 
 == Changelog ==
+
+2.0 Fixed Whitespace 
 
 1.0 Creation
--- soundcloud.php
+++ soundcloud.php
@@ -3,16 +3,13 @@
 Plugin Name: Soundcloud oEmbed
 Plugin URI: http://www.brandonshutter.com
 Description: Enables the ability to paste a SoundCloud URL in it's own line and for oEmbed to make it play.
-Version: 1.1
+Version: 2.0
 Author: Brandon Shutter
 Author URI: http://www.brandonshutter.com
 */
-?>
-
-<?php
+ 
+ 
 function add_oembed_soundcloud(){
-	wp_oembed_add_provider( 'http://soundcloud.com/*', 'http://soundcloud.com/oembed' );
+        wp_oembed_add_provider( 'http://soundcloud.com/*', 'http://soundcloud.com/oembed' );
 }
 add_action('init','add_oembed_soundcloud');
-
-?>
